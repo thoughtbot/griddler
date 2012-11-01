8a22a356fb3172da31392522d3638fd974154019
Joel Oliveira <joel@thoughtbot.com>
2012-10-31 23:04:59 -0400

    JRO & CJT - Flesh out email and config
    Address custom...:
    
    * handler class
    * handler method
    * raw body
    * reply delimeter

--- lib/griddler/email.rb
+++ lib/griddler/email.rb
@@ -4,16 +4,19 @@ class Griddler::Email
   attr_accessor :to, :from, :body, :user, :comment
 
   def initialize(params)
-    if params[:to]
-      @to = extract_address(params[:to], config.to)
-    end
-
-    if params[:from]
-      @from = extract_address(params[:from], :email)
-    end
-
+    @to = extract_address(params[:to], config.to)
+    @from = extract_address(params[:from], :email)
     @subject = params[:subject]
-    @body = extract_body(params[:text], params[:charsets])
+
+    if config.raw_body
+      @body = params[:text]
+    else
+      @body = extract_body(params[:text], params[:charsets])
+    end
+
+    handler_class = config.handler_class
+    handler_method = config.handler_method
+    handler_class.send(handler_method, self)
   end
 
   private
