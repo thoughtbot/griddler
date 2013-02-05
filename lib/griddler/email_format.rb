cd3065bb1d8b7a27340ed2f70eda18f2abe6ea1c
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-02-04 22:00:34 -0500

    Remove unused file

--- lib/griddler/email_format.rb
+++
@@ -1,6 +0,0 @@
-module Griddler::EmailFormat
-  LocalPartSpecialChars = Regexp.escape('!#$%&\'*-/=?+-^_`{|}~')
-  LocalPartUnquoted = '(([[:alnum:]' + LocalPartSpecialChars + ']+[\.\+]+))*[[:alnum:]' + LocalPartSpecialChars + '+]+'
-  LocalPartQuoted = '\"(([[:alnum:]' + LocalPartSpecialChars + '\.\+]*|(\\\\[\u0001-\uFFFF]))*)\"'
-  Regex = Regexp.new('((' + LocalPartUnquoted + ')|(' + LocalPartQuoted + ')+)@(((\w+\-+)|(\w+\.))*\w{1,63}\.[a-z]{2,6})', Regexp::EXTENDED | Regexp::IGNORECASE)
-end
