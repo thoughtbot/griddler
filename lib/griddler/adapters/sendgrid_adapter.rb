f2716d83d4801ea2125c38d2a0185e644337bba1
Joel Oliveira <joel@thoughtbot.com>
2013-02-24 22:51:57 -0500

    [JRO] Clean up the SendGrid adapter class

--- lib/griddler/adapters/sendgrid_adapter.rb
+++ lib/griddler/adapters/sendgrid_adapter.rb
@@ -1,16 +1,31 @@
 module Griddler
   module Adapters
     class SendGridAdapter
+      attr_accessor :params
+
+      def initialize(params)
+        @params = params
+      end
+
       def self.normalize_params(params)
-        attachment_count = params[:attachments].to_i
+        adapter = new(params)
+        adapter.normalize_params
+      end
 
-        attachment_files = attachment_count.times.map do |index|
-          params.delete("attachment#{index + 1}".to_sym)
-        end
-
-        params.delete('attachment-info')
+      def normalize_params
         params[:attachments] = attachment_files
         params
+      end
+
+      private
+
+      def attachment_files
+        params.delete('attachment-info')
+        attachment_count = params[:attachments].to_i
+
+        attachment_count.times.map do |index|
+          params.delete("attachment#{index + 1}".to_sym)
+        end
       end
     end
   end
