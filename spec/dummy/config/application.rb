c956161bee066d9c9e4e6cf4e3d4e9490b08c94d
Caleb Thompson <caleb@calebthompson.io>
2013-07-14 17:48:45 -0400

    Clean things up to pass on Rails 4
    * Test against Rails 4.0 and master
    * Remove ActiveResource require - this wasn't used and was only part of
      the generated dummy spec
    * Remove active_resource/railtie require in spec/dummy
    * Can't test ruby 1.9.2 against rails 4, as that's not supported
      anymore.
    * Remove whitelist_attributes

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -4,9 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 Bundler.require
 require "griddler"
@@ -48,12 +46,6 @@ module Dummy
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
-
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
