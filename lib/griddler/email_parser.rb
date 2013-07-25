fc6ee318020812fa058280c75acadd3c0bab65f5
Randy Schmidt <me@r38y.com>
2013-07-25 14:55:52 -0400

    Factor out the regex so it is clearer.

--- lib/griddler/email_parser.rb
+++ lib/griddler/email_parser.rb
@@ -26,9 +26,7 @@ module Griddler::EmailParser
     if body.blank?
       ""
     else
-      delimiter = Array(Griddler.configuration.reply_delimiter).join('|')
-      regex = %r{#{delimiter}}
-      body.split(regex).first.
+      body.split(reply_delimeter_regex).first.
         split(/^\s*[-]+\s*Original Message\s*[-]+\s*$/).first.
         split(/^\s*--\s*$/).first.
         gsub(/On.*wrote:/, '').
@@ -52,6 +50,11 @@ module Griddler::EmailParser
   end
 
   private
+
+  def self.reply_delimeter_regex
+    delimiter = Array(Griddler.configuration.reply_delimiter).join('|')
+    %r{#{delimiter}}
+  end
 
   def self.extract_email_address(full_address)
     full_address.split('<').last.delete('>').strip
