c510c5fcdcd511583d5060f09093a61ae354b965
Zachary Gohr <zachary.gohr@gmail.com>
2013-10-20 15:03:32 -0500

    From address might not have full name.

--- lib/griddler/email_parser.rb
+++ lib/griddler/email_parser.rb
@@ -56,7 +56,7 @@ module Griddler::EmailParser
   end
 
   def self.extract_email_address(full_address)
-    full_address.split('<').last.delete('>').strip
+    full_address.split('<').last.delete('>').strip if full_address.include?('<') else full_address
   end
 
   def self.extract_name(full_address)
