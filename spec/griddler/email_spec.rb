1d5be8c56f307c1f62ad8ebc5627d806926383e9
gust <gust@Bromine.local>
2014-03-21 15:53:00 -0400

    Adds spec for testing proper parsing of some gmail responses

--- spec/griddler/email_spec.rb
+++ spec/griddler/email_spec.rb
@@ -595,6 +595,18 @@ describe Griddler::Email, 'with custom configuration' do
     end
   end
 
+  describe 'parsing with gmail reply header with newlines' do
+    it "only keeps the message above the reply header" do
+      params[:text]= <<-EOS.strip_heredoc
+This is the real text\r\n\r\n\r\nOn Fri, Mar 21, 2014 at 3:11 PM, Someone <\r\nsomeone@example.com> wrote:\r\n\r\n>  -- REPLY ABOVE THIS LINE --\r\n>\r\n> The Old message!\r\n>\r\n> Another line! *\r\n>\n
+      EOS
+
+      email = Griddler::Email.new(params).process
+      email.body.should eq 'This is the real text'
+    end
+  end
+
+
   describe 'processor_class' do
     it 'calls process on the custom processor class' do
       my_handler = double
