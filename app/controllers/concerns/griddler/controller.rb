50fd46b0f4f0e7e606009fe16be8e56bdac14a69
Ray Zane <ray@promptworks.com>
2016-04-08 15:35:33 -0400

    Extract controller methods into a concern for greater control

---
+++ app/controllers/concerns/griddler/controller.rb
@@ -0,0 +1,31 @@
+module Griddler
+  module Controller
+    extend ActiveSupport::Concern
+
+    included do
+      delegate :processor_class, :processor_method, :email_service, to: :griddler_configuration
+
+      private :processor_class, :processor_method, :email_service
+    end
+
+    private
+
+    def process_griddler
+      normalized_params.each do |p|
+        process_email Griddler::Email.new(p)
+      end
+    end
+
+    def normalized_params
+      Array.wrap(email_service.normalize_params(params))
+    end
+
+    def process_email(email)
+      processor_class.new(email).public_send(processor_method)
+    end
+
+    def griddler_configuration
+      Griddler.configuration
+    end
+  end
+end
--- app/controllers/griddler/emails_controller.rb
+++ app/controllers/griddler/emails_controller.rb
@@ -1,27 +1,8 @@
 class Griddler::EmailsController < ActionController::Base
+  include Griddler::Controller
+
   def create
-    normalized_params.each do |p|
-      process_email Griddler::Email.new(p)
-    end
-
+    process_griddler
     head :ok
-  end
-
-  private
-
-  delegate :processor_class, :processor_method, :email_service, to: :griddler_configuration
-
-  private :processor_class, :processor_method, :email_service
-
-  def normalized_params
-    Array.wrap(email_service.normalize_params(params))
-  end
-
-  def process_email(email)
-    processor_class.new(email).public_send(processor_method)
-  end
-
-  def griddler_configuration
-    Griddler.configuration
   end
 end
