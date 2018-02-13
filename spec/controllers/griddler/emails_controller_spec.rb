115da58123b20ed4b79797237e0cf74273b199b3
lulalala <mark@goodlife.tw>
2018-02-13 10:59:24 +0800

    Change to Rails 5.1 required params

--- spec/controllers/griddler/emails_controller_spec.rb
+++ spec/controllers/griddler/emails_controller_spec.rb
@@ -9,7 +9,7 @@ describe Griddler::EmailsController, :type => :controller do
 
   describe 'POST create', type: :controller do
     it 'is successful' do
-      post :create, email_params
+      post :create, params: email_params
 
       expect(response).to be_success
     end
@@ -20,7 +20,7 @@ describe Griddler::EmailsController, :type => :controller do
         with(hash_including(to: ['tb@example.com'])).
         and_return(email)
 
-      post :create, { to: 'tb@example.com' }
+      post :create, params: { to: 'tb@example.com' }
     end
 
     it 'calls process on the custom processor class' do
@@ -28,7 +28,7 @@ describe Griddler::EmailsController, :type => :controller do
       expect(my_handler).to receive(:new).and_return(my_handler)
       allow(Griddler.configuration).to receive_messages(processor_class: my_handler)
 
-      post :create, email_params
+      post :create, params: email_params
     end
 
     it 'calls the custom processor method on the processor class' do
@@ -38,7 +38,7 @@ describe Griddler::EmailsController, :type => :controller do
       expect(EmailProcessor).to receive(:new).and_return(fake_processor)
       expect(fake_processor).to receive(:perform)
 
-      post :create, email_params
+      post :create, params: email_params
     end
   end
 
