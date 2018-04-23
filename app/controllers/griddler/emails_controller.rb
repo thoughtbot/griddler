7ef52f38e8bd7d76a7ea6aeed81a76ac50d502f4
Joseph Page <josephpage@users.noreply.github.com>
2018-04-23 11:44:57 +0200

    fix ArgumentError with verify_authenticity_token on Rails API

--- app/controllers/griddler/emails_controller.rb
+++ app/controllers/griddler/emails_controller.rb
@@ -1,5 +1,5 @@
 class Griddler::EmailsController < ActionController::Base
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, raise: false
 
   def create
     normalized_params.each do |p|
