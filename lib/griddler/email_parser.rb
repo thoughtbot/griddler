780e22a57d78969436eb1d014175c727e489c725
CodeIgor31 <yagmurov.igor@mail.ru>
2023-08-07 16:21:44 +0300

    Range instead of symbol

--- lib/griddler/email_parser.rb
+++ lib/griddler/email_parser.rb
@@ -93,7 +93,7 @@ module Griddler::EmailParser
 
   def self.remove_reply_portion(body)
     regex_split_points.inject(body) do |result, split_point|
-      result[result.index(split_point)] || ""
+      result[0...result.index(split_point)] || ""
     end
   end
 end
