f634fa4261002778f482999f0cc3079a33c165ba
CodeIgor31 <yagmurov.igor@mail.ru>
2023-08-09 13:01:29 +0300

    Logging params

--- app/controllers/griddler/emails_controller.rb
+++ app/controllers/griddler/emails_controller.rb
@@ -34,7 +34,8 @@ class Griddler::EmailsController < ActionController::Base
         is_griddler: true,
         griddler_from: "params_each_griddler_emails_controller",
         tag: "missing_mail",
-        griddler_params: normalized_params,
+        griddler_params: params,
+        griddler_normalized_params: normalized_params,
         griddler_date: DateTime.now
       }.to_json)
     rescue
